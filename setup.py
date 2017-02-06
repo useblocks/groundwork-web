6f21973514d2a33c4c16ef617f4dfb9ee9e856df
daniel <daniel.woste@useblocks.com>
2017-02-06 14:27:32 +0100

    Changed status to beta

--- setup.py
+++ setup.py
@@ -29,8 +29,10 @@ setup(
                       'pytest-runner', 'sphinx', 'docutils'],
     tests_require=['pytest', 'pytest-flake8'],
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta'
         'Environment :: Console',
+        'Environment :: Web Environment',
+        'Environment :: Plugins'
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
